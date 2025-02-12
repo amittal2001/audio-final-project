7118db9652fb36aaeaefe52f4c28c5bc597fdeec
amittal2001 <talamit2001@gmail.com>
2025-02-12 16:41:06 +0200

    basic one layer nn for example

---
+++ models/OneLayerNN.py
@@ -0,0 +1,11 @@
+
+import torch.nn as nn
+
+# Define a simple one-layer neural network
+class OneLayerNN(nn.Module):
+    def __init__(self, input_dim, num_classes):
+        super(OneLayerNN, self).__init__()
+        self.fc = nn.Linear(input_dim, num_classes)
+
+    def forward(self, x):
+        return self.fc(x)
